0efed09236d4443511ca9ddc122a3413399c144a
One-Two-Smilezgit config --global user.email one.two.smilez@gmail.comgit config --global user.name One-Two-Smilezgit config --global user.name One-Two-Smilez <one.two.smilez@gmail.com>
2014-08-28 11:26:44 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SecondApp::Application.config.secret_key_base = 'c1705f99cd4fa5a72ba5b5570ae07e2ec67eead886a207f39ff7303dd38715380e9c0731cb7a65fd8add56ac5bd6812f73a635820b7aef55b28e8f6cb44f368a'
